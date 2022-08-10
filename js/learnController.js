9c60d3cb17d9da739aa6745affbec2b714b55606
Mykala Lowe <102772996+MykalaLowe@users.noreply.github.com>
2022-08-10 15:25:46 -0500

    Add files via upload

---
+++ js/learnController.js
@@ -0,0 +1,25 @@
+/**
+ * access the previously created module 'musicProject'
+ */
+
+
+
+(function() {
+	var myMusicProject = angular.module('musicProject');
+
+	myMusicProject.controller('learnController', function($scope) {
+	
+		/*		$scope.openNav = function() {
+			document.getElementById("mySidebar").style.width = "250px";
+			document.getElementById("main").style.marginLeft = "250px";
+		}
+		
+		$scope.closeNav = function() {
+			document.getElementById("mySidebar").style.width = "0";
+			document.getElementById("main").style.marginLeft= "0";
+		}
+	*/
+	});
+	
+})();
+
